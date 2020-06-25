b2747c7611dd32ff670de08d11b5b622fe8fcd25
AnnCoding <18292683306@163.com>
2020-06-25 09:08:17 +0800

    while 改成for循环

--- src/AnnCoding/RmbSeq.java
+++ src/AnnCoding/RmbSeq.java
@@ -20,8 +20,10 @@ import com.intellij.psi.PsiNameValuePair;
 import com.intellij.ui.CollectionListModel;
 import org.apache.commons.lang.StringUtils;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author chenjiena
@@ -47,7 +49,8 @@ public class RmbSeq extends AnAction {
         WriteCommandAction.runWriteCommandAction(psiMethod.getProject(), new Runnable() {
             @Override
             public void run() {
-                RmbSeq.this.addRmbSubCommandMessage(psiMethod);
+                //                RmbSeq.this.addRmbSubCommandMessage(psiMethod);
+                Map<Integer, Character> markMap = new HashMap<>();
                 RmbSeq.this.addRmbField(psiMethod);
             }
         });
@@ -72,7 +75,6 @@ public class RmbSeq extends AnAction {
         psiClass.getModifierList().addAnnotation(buildRmbMessage());
     }
 
-
     /**
      * @return
      * @RmbSubCommandMessage( subCommand = "",
@@ -93,43 +95,65 @@ public class RmbSeq extends AnAction {
         return sb.toString();
     }
 
+    /**
+     * 1. 用map 将父类和子类所有的序号都存起来
+     * 2. 取到RMBField 的内容之后先做去空格和去回车保证取到顺序
+     *
+     * @param psiClass
+     */
+
     private void addRmbField(PsiClass psiClass) {
+        Map<Integer, Character> markMap = new HashMap<>();
         List<PsiField> fields = (new CollectionListModel(psiClass.getFields())).getItems();
         if (fields != null) {
-            int count = this.checkSuperFieldCount(psiClass);
-            count = Math.max(count, this.getMaxSeqMaxValue(fields)) + 1;
+            checkSuperFieldCount(psiClass, markMap);//子类最大
+            setMarkMapValue(fields, markMap);
             Iterator var8 = fields.iterator();
 
             //遍历字段
+            Integer i = 1;
             while (var8.hasNext()) {
                 PsiField field = (PsiField) var8.next();
                 if (!this.ignoreField(field)) {
-                    field.getModifierList().addAnnotation(buildRmbField(count));
-                    ++count;
+                    while (markMap.get(i) != null && ReflectUtil.MARK.equals(markMap.get(i))) {
+                        ++i;
+                    }
+                    field.getModifierList().addAnnotation(buildRmbField(i));
+                    markMap.put(i, ReflectUtil.MARK);
                 }
             }
         }
     }
 
-    private Integer getMaxSeqMaxValue(List<PsiField> list) {
-        Integer maxValue = -2147483648;
-        Iterator var3 = list.iterator();
+    private void setMarkMapValue(List<PsiField> list, Map<Integer, Character> markMap) {
+        if (list.isEmpty()) {
+            return;
+        }
+        PsiAnnotationParameterList parameterList = null;
+        list.forEach(psiField -> {
+            PsiAnnotation annotation = this.findAnnotationOnField(psiField, "cn.webank.weup.rmb.annotation.RmbField");
+            if (annotation != null) {
+                parameterList.add(annotation.getParameterList());
+            }
 
-        while (true) {
-            PsiAnnotationParameterList parameterList;
-            do {
-                PsiAnnotation annotation;
-                do {
-                    if (!var3.hasNext()) {
-                        return maxValue.equals(-2147483648) ? 0 : maxValue;
-                    }
+        });
 
-                    PsiField m = (PsiField) var3.next();
-                    annotation = this.findAnnotationOnField(m, "cn.webank.weup.rmb.annotation.RmbField");
-                } while (annotation == null);
-
-                parameterList = annotation.getParameterList();
-            } while (parameterList == null);
+//        Iterator fieldIterator = list.iterator();
+//        while (true) {
+//            PsiAnnotationParameterList parameterList;
+//            do {
+//                PsiAnnotation annotation;
+//                do {
+//                    if (!fieldIterator.hasNext()) {
+//                        return;
+//                    }
+//
+//                    PsiField m = (PsiField) fieldIterator.next();
+//                    annotation = this.findAnnotationOnField(m, "cn.webank.weup.rmb.annotation.RmbField");
+//                } while (annotation == null);
+//
+//                parameterList = annotation.getParameterList();
+//            } while (parameterList == null);
 
             PsiNameValuePair[] var7 = parameterList.getAttributes();
             int var8 = var7.length;
@@ -137,11 +161,10 @@ public class RmbSeq extends AnAction {
             for (int var9 = 0; var9 < var8; ++var9) {
                 PsiNameValuePair e = var7[var9];
                 String value = ReflectUtil.getSeq(e.getValue().getText());
-                if (e.getValue() != null && StringUtils.isNotBlank(value) && ReflectUtil.isNumeric(value)) {
-                    maxValue = Math.max(maxValue, Integer.parseInt(value));
+                if (StringUtils.isNotBlank(value) && ReflectUtil.isNumeric(value)) {
+                    markMap.put(Integer.valueOf(value), ReflectUtil.MARK);
                 }
             }
-        }
     }
 
     /**
@@ -154,33 +177,31 @@ public class RmbSeq extends AnAction {
         return sb.toString();
     }
 
-    private Integer checkSuperFieldCount(PsiClass psiClass) {
+    private Integer checkSuperFieldCount(PsiClass psiClass, Map<Integer, Character> markMap) {
         Integer maxValue = -2147483648;
         PsiClass[] supers = psiClass.getSupers();
         PsiClass[] var4 = supers;
         int superLength = supers.length;
 
-        for(int i = 0; i < superLength; ++i) {
+        for (int i = 0; i < superLength; ++i) {
             PsiClass su = var4[i];
             List<PsiField> list = (new CollectionListModel(psiClass.getFields())).getItems();
-            Integer value = this.getMaxRmbFieldMaxValue(list);
-            maxValue = Math.max(maxValue, value);
-            value = this.checkSuperFieldCount(su);
-            maxValue = Math.max(maxValue, value);
+            setMarkMapValue(list, markMap);
+            checkSuperFieldCount(su, markMap);
         }
 
         return maxValue;
     }
 
     private boolean ignoreField(PsiField field) {
-        return field.getModifierList().hasModifierProperty("final") || field.getModifierList().hasModifierProperty("static");
+        return field.getModifierList().hasModifierProperty("final") || field.getModifierList()
+                .hasModifierProperty("static")
+                || findAnnotationOnField(field, "cn.webank.weup.rmb.annotation.RmbField") != null;
     }
 
-
-
     private PsiElement getPsiElement(AnActionEvent e) {
-        PsiFile psiFile = (PsiFile)e.getData(LangDataKeys.PSI_FILE);
-        Editor editor = (Editor)e.getData(PlatformDataKeys.EDITOR);
+        PsiFile psiFile = (PsiFile) e.getData(LangDataKeys.PSI_FILE);
+        Editor editor = (Editor) e.getData(PlatformDataKeys.EDITOR);
         if (psiFile != null && editor != null) {
             int offset = editor.getCaretModel().getOffset();
             return psiFile.findElementAt(offset);
@@ -190,44 +211,12 @@ public class RmbSeq extends AnAction {
         }
     }
 
-    private Integer getMaxRmbFieldMaxValue(List<PsiField> list) {
-        Integer maxValue = -2147483648;
-        Iterator var3 = list.iterator();
-
-        while(true) {
-            PsiAnnotationParameterList parameterList;
-            do {
-                PsiAnnotation annotation;
-                do {
-                    if (!var3.hasNext()) {
-                        return maxValue.equals(-2147483648) ? 0 : maxValue;
-                    }
-
-                    PsiField m = (PsiField)var3.next();
-                    annotation = this.findAnnotationOnField(m, "cn.webank.weup.rmb.annotation.RmbField");
-                } while(annotation == null);
-
-                parameterList = annotation.getParameterList();
-            } while(parameterList == null);
-
-            PsiNameValuePair[] var7 = parameterList.getAttributes();
-            int var8 = var7.length;
-
-            for(int var9 = 0; var9 < var8; ++var9) {
-                PsiNameValuePair e = var7[var9];
-                if (e.getValue() != null && ReflectUtil.isNumeric(e.getValue().getText())) {
-                    maxValue = Math.max(maxValue, Integer.parseInt(e.getValue().getText()));
-                }
-            }
-        }
-    }
-
     private PsiAnnotation findAnnotationOnField(PsiField psiField, String annotationName) {
         PsiModifierList modifierList = psiField.getModifierList();
         PsiAnnotation[] var4 = modifierList.getAnnotations();
         int var5 = var4.length;
 
-        for(int var6 = 0; var6 < var5; ++var6) {
+        for (int var6 = 0; var6 < var5; ++var6) {
             PsiAnnotation psiAnnotation = var4[var6];
             if (annotationName.equals(psiAnnotation.getQualifiedName())) {
                 return psiAnnotation;
--- src/AnnCoding/Utils/ReflectUtil.java
+++ src/AnnCoding/Utils/ReflectUtil.java
@@ -12,27 +12,46 @@ public final class ReflectUtil {
 
     private static final Pattern pattern = Pattern.compile("[0-9]*");
 
+    public static final Character MARK = '1';
+
     public ReflectUtil() {
     }
 
+    /**
+     * \n 回车(\u000a)
+     * \t 水平制表符(\u0009)
+     * \s 空格(\u0008)
+     * \r 换行(\u000d)
+     */
+    private static String replaceAllBlank(String str) {
+        String s = "";
+        if (str != null) {
+            Pattern p = Pattern.compile("\\s*|\t|\r|\n");
+            Matcher m = p.matcher(str);
+            s = m.replaceAll("");
+        }
+        return s;
+    }
 
     public static String getSeq(String url) {
+        url = replaceAllBlank(url);
         String moneyText = "";
-        Pattern p = Pattern.compile("seq = " + "[0-9]*");
+        Pattern p = Pattern.compile("seq=" + "[0-9]*");
         Matcher m = p.matcher(url);
         //如果字符串中存在这种类型的字符串就把这个字符串截取出来
         if (m.find()) {
             moneyText = m.group();
-            int indexOf = moneyText.indexOf("seq = ");
+            int indexOf = moneyText.indexOf("seq=");
             //截取出字符串后面的数字
-            moneyText = moneyText.substring(indexOf + 6, moneyText.length());
+            moneyText = moneyText.substring(indexOf + 4, moneyText.length());
         }
         return moneyText;
     }
 
     public static void main(String[] args) {
-        String rmb = "seq = 2, title = \"ecifNo\", remark = \"ecif号\"";
-        System.out.println(getSeq(rmb));
+        String s = "RmbField(seq = 22, title = \"ecifNo\", remark = \"ecif号\")";
+        System.out.println(s);
+        System.out.println(getSeq(s));
     }
 
     public static boolean isNumeric(String str) {
